9fbccadcba6ca8726adb57512ec61df82dddc6b3
Tazz <104685150+ItsMeTazz@users.noreply.github.com>
2023-08-22 15:58:48 -0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -62,15 +62,16 @@ export default function Home() {
 
           <div className="flex gap-10 mt-5">
             <a
-              href="https://basescan.org/token/0xab8a1c03b8e4e1d21c8ddd6edf9e07f26e843492?a=0x000000000000000000000000000000000000dead"
+              //href="https://basescan.org/token/0xab8a1c03b8e4e1d21c8ddd6edf9e07f26e843492?a=0x000000000000000000000000000000000000dead"
               target="_blank"
               className="flex gap-2"
             >
               <GiBurningMeteor size={45} className="text-[#cb5f18]" />
               <div className="flex flex-col">
                 <div>$DOPA BURNED</div>
-                <div className="text-[#cb5f18]">
-                  {deadSupply && (
+                <div className="text-[#cb5f18] font-bold">
+                  COMING SOON
+                  {/* {deadSupply && (
                     <div className="">
                       {Number(deadSupply?.formatted).toFixed(4)} /{" "}
                       {Number(
@@ -78,20 +79,21 @@ export default function Home() {
                       ).toFixed(2)}
                       %
                     </div>
-                  )}
+                  )} */}
                 </div>
               </div>
             </a>
             <a className="flex gap-2">
-              <GiCoins size={45} className="text-moon" />
+              <GiCoins size={45} className="text-moon font-bold" />
               <div className="flex flex-col">
                 <div>$DOPA TOTAL SUPPLY</div>
                 <div className="text-moon">
-                  {totalSupply && deadSupply && (
+                  COMING SOON
+                  {/* {totalSupply && deadSupply && (
                     <div className="">
                       {(Number(totalSupply) - Number(deadSupply.formatted)).toFixed(2)}
                     </div>
-                  )}
+                  )} */}
                 </div>
               </div>
             </a>
