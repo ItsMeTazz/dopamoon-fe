f04f689daf3247142c5f7dc4ff266324c49b35bf
Tazz <104685150+ItsMeTazz@users.noreply.github.com>
2023-08-25 16:42:28 -0700

    Update web2Context.tsx

--- src/contexts/web2Context.tsx
+++ src/contexts/web2Context.tsx
@@ -9,7 +9,7 @@ import {
 
 export type Web2ContextType = {
   dopamoonPrice: Number;
-  bonePrice: Number;
+  ethPrice: Number;
 };
 
 export const Web2Context = createContext<Web2ContextType | null>(null);
@@ -27,47 +27,19 @@ export default function Web2Provider({ children }: Props) {
 
   useEffect(() => {
     async function fetchPrices() {
-      const query = {
-        query: `
-          {
-            getTokenPrices(inputs: [
-              { address: "0x9813037ee2218799597d83d4a5b6f3b6778218d9", networkId: 1 },
-              { address: "0xB0cb6dE25BFc5811E323DBF0495d9BA6A154f43a", networkId: 109 }
-            ]) {
-              priceUsd
-            }
-          }
-        `,
-      };
+      const req = await fetch(
+        "https://api.dexscreener.com/latest/dex/pairs/shibarium/0x4a89dbcf583f899371ca9dacd9a9840202caf160",
+        { next: { revalidate: 10 } }
+      );
+      const priceData = await req.json();
+      setDopaPrice(Number(priceData.pairs[0].priceUsd));
 
-      const reqResponse = await fetch("https://api.defined.fi/", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          "x-api-key": process.env.NEXT_PUBLIC_DEFINED_KEY
-            ? process.env.NEXT_PUBLIC_DEFINED_KEY
-            : "",
-        },
-        body: JSON.stringify(query),
-      });
-
-      const res = await reqResponse.json();
-
-      setBonePrice(res.data.getTokenPrices[0].priceUsd);
-      setDopaPrice(res.data.getTokenPrices[1].priceUsd);
-      // const req = await fetch(
-      //   "https://api.dexscreener.com/latest/dex/pairs/avalanche/0x640a8a1ca1de9457d72896e5fecb1392dd9efdfa",
-      //   { next: { revalidate: 30 } }
-      // );
-      // const priceData = await req.json();
-      // setDopaPrice(Number(priceData.pairs[0].priceUsd));
-
-      // const reqEth = await fetch(
-      //   "https://api.dexscreener.com/latest/dex/pairs/ethereum/0xb011e4eb4111ef00b620a5ed195836dcd69db1ff",
-      //   { next: { revalidate: 30 } }
-      // );
-      // const priceDataEth = await reqEth.json();
-      // setBonePrice(Number(priceDataEth.pairs[0].priceUsd));
+      const reqEth = await fetch(
+        "https://api.dexscreener.com/latest/dex/pairs/shibarium/0x59bffa3e7b23bd0eacf1ba03a8865ebf1f085263",
+        { next: { revalidate: 10 } }
+      );
+      const priceDataEth = await reqEth.json();
+      setBonePrice(Number(priceDataEth.pairs[0].priceUsd));
     }
 
     fetchPrices();
@@ -77,7 +49,7 @@ export default function Web2Provider({ children }: Props) {
     <Web2Context.Provider
       value={{
         dopamoonPrice: dopaPrice,
-        bonePrice: bonePrice,
+        ethPrice: bonePrice,
       }}
     >
       {children}
